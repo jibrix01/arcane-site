6c837b3b4a36abaa7063353ecb0787485caabb42
chry607 <138194861+chry607@users.noreply.github.com>
2026-07-27 00:26:39 +0800

    Fixed hardcoded navbar

---
+++ js/load-nav.js
@@ -0,0 +1,10 @@
+async function loadComponent(id, file) {
+  const element = document.getElementById(id);
+
+  const response = await fetch(file);
+  const html = await response.text();
+
+  element.innerHTML = html;
+}
+
+loadComponent("nav-placeholder", "components/nav.html");
